631a537238587416bcc5fa2eb55ea7ef98e1261f
mekdi1238 <megdelawitabrham0@gmail.com>
2026-04-26 18:13:39 +0300

    Implemented CRUD operations with file storage

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Todo List API
-This is a REST API project built using Node.js core modules(http and fs).
+# Todo API (Node.js)
+
+## Description
+A simple REST API built using Node.js core modules (http and fs) that performs CRUD operations.
+
+## Features
+- Create todo
+- Read all todos
+- Read todo by ID
+- Update todo
+- Delete todo
+
+## Tech Used
+- Node.js
+- http module
+- fs module
+- JSON file storage
+
+## How to run
+node server.js
--- server.js
+++ server.js
@@ -1,10 +1,126 @@
 const http = require("http");
+const fs = require("fs");
+
 const PORT = 3000; 
+const DATA_FILE ="./data.json";
+
 const server = http.createServer((req,res) => {
-    res.writeHead(200, {"content-type": "application/json"});
+    res.setHeader("Content-type", "application/json");
+
+ if(req.url === "/" && req.method === "GET"){
+    res.statusCode= 200;
     res.end(JSON.stringify({message: "Todo API is running"}));
+}
+else if(req.url === "/todos" && req.method === "GET"){
+    const todos = readTodos();
+    res.statusCode= 200;
+    res.end(JSON.stringify(todos));
+}
+else if (req.url.startsWith("/todos/") && req.method === "GET") {
+    const id = parseInt(req.url.split("/")[2]);
+    const todos = readTodos();
+
+    const todo = todos.find(t => t.id === id);
+
+    if (!todo) {
+        res.statusCode = 404;
+        return res.end(JSON.stringify({ message: "Todo not found" }));
+    }
+
+    res.statusCode = 200;
+    res.end(JSON.stringify(todo));
+}
+else if (req.url.startsWith("/todos/") && req.method === "PUT") {
+    const id = parseInt(req.url.split("/")[2]);
+    let body = "";
+
+    req.on("data", (chunk) => {
+        body += chunk.toString();
+    });
+
+    req.on("end", () => {
+        try {
+            const updatedData = JSON.parse(body);
+            const todos = readTodos();
+
+            const index = todos.findIndex(t => t.id === id);
+
+            if (index === -1) {
+                res.statusCode = 404;
+                return res.end(JSON.stringify({ message: "Todo not found" }));
+            }
+
+            // Update fields if provided
+            if (updatedData.task !== undefined) {
+                todos[index].task = updatedData.task;
+            }
+
+            if (updatedData.completed !== undefined) {
+                todos[index].completed = updatedData.completed;
+            }
+
+            writeTodos(todos);
+
+            res.statusCode = 200;
+            res.end(JSON.stringify(todos[index]));
+        }
+        catch (err) {
+            res.statusCode = 400;
+            res.end(JSON.stringify({ message: "Invalid JSON" }));
+        }
+    });
+}
+
+
+else if(req.url === "/todos" && req.method === "POST"){
+    let body = "";
+
+    req.on("data", (chunk) => {
+        body += chunk.toString();
+    });
+
+    req.on("end", () => {
+        try{
+            const newTodo = JSON.parse(body);
+            if(!newTodo.task){
+                res.statusCode = 400;
+                return res.end(JSON.stringify({message: "Task is required"}));
+            }
+            const todos = readTodos();
+
+            const todo = {
+                id: todos.length>0 ? todos[todods.length - 1].id+ 1 : 1,
+                task: newTodo.task,
+                completed:false
+            };
+            todos.push(todo);
+            writeTodos(todos);
+
+            res.statusCode = 201;
+            res.end(JSON.stringify(todo));
+        }
+        catch(err){
+            res.statusCode = 400;
+            res.end(JSON.stringify({message: "Invalid JSON"}));
+        }
+    });
+}
+else{
+    res.statusCode = 404;
+    res.end(JSON.stringify({error: "Route not found"}));
+}
+});
+function readTodos(){
+const data = fs.readFileSync(DATA_FILE, "utf8");
+return JSON.parse(data);
+}
+
+function writeTodos(todos){
+    fs.writeFileSync(DATA_FILE, JSON.stringify(todos, null, 2));
+}
+
+server.listen(PORT, () =>{
+console.log(`Server running on http://localhost:${PORT}`)
 });
 
-server.listen(PORT, ()=> {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+
